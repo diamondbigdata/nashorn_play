38dc0ab65e97d051ce6c7f8f4d42dfb62c6055ff
Rob Anderson <roboprog@yahoo.com>
2015-11-26 21:04:12 -0800

    script which uses native Java objects

---
+++ script/np_script/src/js/java_user.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env jjs -scripting
+
+// JavaScript script which also makes use of Java (1.8) code.
+//  
+//  Usage:
+//          java_user.js [ -- number number number ... ]
+//      example: java_user.js -- .1 .2
+//      (see http://0.30000000000000004.com/, http://roboprogs.com/devel/2010.02.html)
+// .....................................
+
+'use strict'
+
+/**
+ * An example JS object (singleton) used as a namespace
+ * @namespace
+ */
+var ns = {
+
+    /** take numbers from command line, and add them up */
+    run: function ( argv) {
+        var acc
+
+        acc = ns.create_number( '0.0' )
+        argv.forEach( function ( num_str) {
+            acc = acc.add( ns.create_number( num_str ) )
+        } )
+        print( 'Total: ' + acc + '\n')
+    },
+
+    /**
+     * Factory method for fixed precision numbers.
+     * @param num_str {string} - numeric value as a string
+     * @return java.math.BigDecimal
+     */
+    create_number: function ( num_str ) {
+        return new java.math.BigDecimal( num_str )
+    }
+
+}
+
+ns.run( $ARG)
+
+
+// vi: ts=4 sw=4 expandtab ai
+// *** EOF ***
