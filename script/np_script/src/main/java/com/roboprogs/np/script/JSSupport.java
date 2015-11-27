de8ee04e8766ea794a6fa06a8f2a35b38e0774c6
Rob Anderson <roboprog@yahoo.com>
2015-11-27 12:22:48 -0800

    overload jprint method, including ScriptObjectMirror version

--- script/np_script/src/main/java/com/roboprogs/np/script/JSSupport.java
+++ script/np_script/src/main/java/com/roboprogs/np/script/JSSupport.java
@@ -1,5 +1,8 @@
 package com.roboprogs.np.script;
 
+import java.util.*;
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
+
 /**
  * Base class to interact with a JavaScript script.
  */
@@ -33,6 +36,24 @@ public abstract class JSSupport {
 	/** print stuff, Java style */
 	protected void jprint( String s) {
 		System.out.println( "[Instance " + this.data +  "] JAVALAND print: " + s);
+	}
+
+	/** print stuff, Java style, but with JavaScript input */
+	protected void jprint( ScriptObjectMirror o) {
+        Map <String, Object> props;
+
+        props = new TreeMap <> ( o );
+        jprint( props.toString() );
+	}
+
+	/** print stuff, Java style, but with JavaScript input */
+	protected void jprint(
+            String intro,
+            ScriptObjectMirror o) {
+        Map <String, Object> props;
+
+        props = new TreeMap <> ( o );
+        jprint( intro + ":" + props.toString() );
 	}
 
 }
