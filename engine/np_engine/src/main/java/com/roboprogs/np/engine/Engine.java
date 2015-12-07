e5add1568e01d804c0435a62b4074f9559c4489b
Rob Anderson <roboprog@yahoo.com>
2015-12-07 13:38:29 -0800

    first cut at loading and running a script engine

--- engine/np_engine/src/main/java/com/roboprogs/np/engine/Engine.java
+++ engine/np_engine/src/main/java/com/roboprogs/np/engine/Engine.java
@@ -1,13 +1,24 @@
 package com.roboprogs.np.engine;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 /**
- * Hello world!
- *
+ * Load the Nashorn scripting engine and run a simple script.
  */
-public class Engine 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Engine {
+
+    public static void main( String[] args ) throws Throwable {
+        ScriptEngine engine;
+
+        engine = ( new ScriptEngineManager() ).getEngineByName( "nashorn" );
+        engine.eval( "var x = { hello: 'world' }" ) );
+        engine.eval( "print( x ) + '\\n'" );
+        System.out.println( engine.eval( "x" ) );
     }
+
 }
+
+
+// vi: ts=4 sw=4 expandtab
+// *** EOF ***
