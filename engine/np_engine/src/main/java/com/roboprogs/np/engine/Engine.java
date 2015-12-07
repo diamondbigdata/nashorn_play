be8d66c0d9528b5bfad633dfb789417af133a6b2
Rob Anderson <roboprog@yahoo.com>
2015-12-07 13:20:25 -0800

    Maven project stub

---
+++ engine/np_engine/src/main/java/com/roboprogs/np/engine/Engine.java
@@ -0,0 +1,13 @@
+package com.roboprogs.np.engine;
+
+/**
+ * Hello world!
+ *
+ */
+public class Engine 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
