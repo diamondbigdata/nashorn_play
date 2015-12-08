90b6cf573231d09a5f5dcc87d6b48bee3577ab16
Rob Anderson <roboprog@yahoo.com>
2015-12-07 23:20:37 -0800

    WIP - load external script files, so that they can be debugged

---
+++ engine/np_engine/src/main/java/com/roboprogs/np/engine/Debug.java
@@ -0,0 +1,48 @@
+package com.roboprogs.np.engine;
+
+import java.io.*;
+import javax.script.ScriptEngineManager;
+import jdk.nashorn.api.scripting.NashornScriptEngine;
+
+/**
+ * Load the Nashorn scripting engine,
+ *  and then load a script so that it could be run under debugger.
+ */
+public class Debug {
+
+    public static void main( String[] args ) throws Throwable {
+        NashornScriptEngine engine;
+        Object status;
+        MyRunner tsOps;
+
+        engine = (NashornScriptEngine) ( new ScriptEngineManager() ) .
+                getEngineByName( "nashorn" );
+        // engine.compile( new FileReader( "../../script/np_script/src/js/moment.js") );
+        status = engine.eval( new FileReader( "../../script/np_script/src/js/moment.js") );
+        status = engine.eval( new FileReader( "src/js/js_dep.js") );
+        // TODO: close files
+
+/*
+        tsOps = engine.getInterface( MyRunner.class );
+        tsOps.run( null, 0, null);
+        tsOps.run( "MM/DD/YYYY", 0, null);
+        tsOps.run( "Do MMMM, YYYY", 7, "day");
+        tsOps.run( "MM/DD/YYYY HH:mm", -4, "hour");
+*/
+
+        engine.eval( "dep.run()" );
+    }
+
+    /** dummy interface to fish up the function we want in js_user.js */
+    private interface MyRunner {
+
+        public void run( Object format, Object increment, Object unit ) 
+        ;
+
+    }
+
+}
+
+
+// vi: ts=4 sw=4 expandtab
+// *** EOF ***
