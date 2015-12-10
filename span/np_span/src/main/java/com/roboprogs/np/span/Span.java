f6d5709b5bcbbf1b615387b65b1786a0d165bc2d
Rob Anderson <roboprog@yahoo.com>
2015-12-10 14:10:13 -0800

    add JS engine, script stub

--- span/np_span/src/main/java/com/roboprogs/np/span/Span.java
+++ span/np_span/src/main/java/com/roboprogs/np/span/Span.java
@@ -1,14 +1,28 @@
 package com.roboprogs.np.span;
 
+import java.io.*;
+import javax.script.ScriptEngineManager;
+import jdk.nashorn.api.scripting.NashornScriptEngine;
+
 import spark.Spark;
 
+
 /**
- * Hello world!
+ * Demo SNAP web stack.
  */
 public class Span {
 
+    /**
+     * Nashorn JS interpreter/JIT.
+     * Assume it is thead-safe, for now.
+     */
+    public static final NashornScriptEngine engine =
+            (NashornScriptEngine) ( new ScriptEngineManager() ) .
+                getEngineByName( "nashorn" );
+
     /** program entry point */
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws Exception {
+        loadScript( "/js/snap.js" );
         Spark.get( "/hello", ( req, res ) -> {
             String name;
 
@@ -17,6 +31,17 @@ public class Span {
                     name :
                     "World" );
         } );
+    }
+
+    /** Run (load) the indicated Javascript (resource) */
+    private static void loadScript( String fname ) throws Exception {
+        Reader in;
+
+        // TODO: proper error handling
+        in = new BufferedReader( new InputStreamReader(
+                Span.class.getResourceAsStream( fname ) ) );
+        Span.engine.eval( in );
+        in.close();;
     }
 
 }
---
+++ span/np_span/src/main/resources/js/snap.js
@@ -0,0 +1,1 @@
+print( 'TODO - access java web support code' )
