03619737c169f5901fc714af5656cbec022a2945
Rob Anderson <roboprog@yahoo.com>
2015-12-09 21:58:07 -0800

    add "SPAN" stub

---
+++ span/np_span/src/main/java/com/roboprogs/np/span/App.java
@@ -0,0 +1,13 @@
+package com.roboprogs.np.span;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
