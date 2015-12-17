ae1e9c1273ba0c2ebc1bb968677c088ce5ec8116
Rob Anderson <roboprog@yahoo.com>
2015-12-17 14:06:02 -0800

    set up a Postgres data-source

--- snap/np_snap/src/main/java/com/roboprogs/np/snap/Snap.java
+++ snap/np_snap/src/main/java/com/roboprogs/np/snap/Snap.java
@@ -1,8 +1,13 @@
 package com.roboprogs.np.snap;
 
 import java.io.*;
+import java.sql.Driver;
+import java.sql.DriverManager;
 import javax.script.ScriptEngineManager;
+import javax.sql.DataSource;
 import jdk.nashorn.api.scripting.NashornScriptEngine;
+
+import org.postgresql.ds.PGPoolingDataSource;
 
 import spark.Spark;
 
@@ -26,6 +31,9 @@ public class Snap {
         // e.g. - view localhost:4567/html/index.html
         Spark.staticFileLocation( "/public" );
 
+        // TODO: get and use a DB connection...
+        getDataSource().getConnection();
+
         loadScript( "/js/snap.js" );
         Spark.get( "/hello", ( req, res ) -> {
             String name;
@@ -45,7 +53,35 @@ public class Snap {
         in = new BufferedReader( new InputStreamReader(
                 Snap.class.getResourceAsStream( fname ) ) );
         Snap.engine.eval( in );
-        in.close();;
+        in.close();
+    }
+
+    /** Get a datasource for connection. */
+    private static DataSource getDataSource() {
+        // TODO: configure externally
+        final String DB_HOST = "localhost";
+        final int DB_PORT = 5432;
+        final String DB_NAME = "snap_demo_db";
+        final String DB_USER = "app";
+        final String DB_PWD = "cryptic";
+
+        PGPoolingDataSource ds;
+
+        try {
+            DriverManager.registerDriver( (Driver)
+                    Class.forName( "org.postgresql.Driver" ).newInstance() );
+            ds = new PGPoolingDataSource();
+            ds.setServerName( DB_HOST );
+            ds.setPortNumber( DB_PORT );
+            ds.setDatabaseName( DB_NAME );
+
+            ds.setUser( DB_USER );
+            ds.setPassword( DB_PWD );
+            ds.setMaxConnections( 100 );  // default was less than 100
+            return ds;
+        } catch ( Throwable e) {
+            throw new RuntimeException( "Datasource fetch failed", e );
+        }
     }
 
 }
